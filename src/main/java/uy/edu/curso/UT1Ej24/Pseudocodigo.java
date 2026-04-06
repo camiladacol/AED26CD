062a1778de03f0fc278d44f2e75e4eb318c692a0
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-04-06 11:05:37 -0300

    Inicio ejercicio 24

---
+++ src/main/java/uy/edu/curso/UT1Ej24/Pseudocodigo.java
@@ -0,0 +1,19 @@
+package uy.edu.curso.UT1Ej24;
+
+public class Pseudocodigo
+{
+    /*
+    funcion union (lista1, lista2):
+
+    crear listaFinal vacía
+
+    para cada elemento x en lista1:
+        agregar x a listaFinal
+
+    para cada elemento x en lista2:
+        si x NO está en listaFinal:
+            agregar x a listaFinal
+
+    retornar listaFinal
+     */
+}
