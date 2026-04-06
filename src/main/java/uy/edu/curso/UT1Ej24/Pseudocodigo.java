76fced46709822bbbc69731360ba4883cb267faf
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-04-06 15:30:22 -0300

    UT1 - Ejercicio 19 terminado

--- src/main/java/uy/edu/curso/UT1Ej24/Pseudocodigo.java
+++ src/main/java/uy/edu/curso/UT1Ej24/Pseudocodigo.java
@@ -3,17 +3,28 @@ package uy.edu.curso.UT1Ej24;
 public class Pseudocodigo
 {
     /*
-    funcion union (lista1, lista2):
-
-    crear listaFinal vacía
-
-    para cada elemento x en lista1:
-        agregar x a listaFinal
-
-    para cada elemento x en lista2:
-        si x NO está en listaFinal:
+    funcion union(lista1, lista2):
+        crear listaFinal vacía
+        para cada elemento x en lista1:
             agregar x a listaFinal
+        para cada elemento x en lista2:
+            si x NO está en listaFinal:
+             agregar x a listaFinal
+        retornar listaFinal(lista1, lista2):
 
-    retornar listaFinal
+    funcion interseccion
+        crear listaFinal vacía
+        para cada elemento x en lista1:
+            si x está en lista2:
+             agregar x a listaFinal
+        retornar listaFinal
+
+    Caso de prueba:
+
+    Conjunto A={1,2,3}
+    Conjunto B={3,4,5}
+    Union = {1,2,3,4,5}
+    Intersección = {3}
      */
+
 }
